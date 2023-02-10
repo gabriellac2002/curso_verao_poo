3fbe56eecd26c54a64f1b5afe5448e9fd8247b37
cidadcruz cida <gabriellacsilva2002@gmail.com>
2023-02-10 01:25:19 -0300

    Ultimo commit nesse repositorio

--- ColecoesGenerics/src/MinhaClasse.java
+++ ColecoesGenerics/src/MinhaClasse.java
@@ -1,6 +1,7 @@
 
 public class MinhaClasse {
 	public int num;
+	public Double num1;
 	
 	public MinhaClasse(int num) {
 		this.num = num;
@@ -8,5 +9,13 @@ public class MinhaClasse {
 	
 	public int aoQuadrado() {
 		return num * num;
+	}
+	
+	public MinhaClasse(Double num) {
+		this.num1 = num1;
+	}
+	
+	public Double aoQuadradoDouble() {
+		return num1 * num1;
 	}
 }
--- ColecoesGenerics/src/MinhaClasseTeste.java
+++ ColecoesGenerics/src/MinhaClasseTeste.java
@@ -4,6 +4,7 @@ public class MinhaClasseTeste {
 	public static void main(String[] args) throws Exception{
 		MinhaClasse minhaClasse = new MinhaClasse(2);
 		System.out.println(minhaClasse.aoQuadrado());
+		System.out.println(minhaClasse.aoQuadradoDouble());
 
 	}
 
